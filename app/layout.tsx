716680c7f21b2dc8864ab533dc4e2c48ba3b8ae7
Ziya Akhundov <akhundovziya04@gmail.com>
2024-03-03 10:46:33 +0400

    first

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import "./globals.css";
+import Favicon from "../public/logo.svg";
+import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const IBMPlex = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-ibm-plex",
+});
+
+export const metadata: Metadata = {
+  title: "PS Photo AI",
+  description: "AI-powered image editor designed by programmers school",
+  icons: [{ rel: "icon", url: Favicon.src }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
