f7ce42c1d4753b593dbb30c88904157946cd0798
Ziya Akhundov <akhundovziya04@gmail.com>
2024-03-17 23:34:41 +0400

    vercel speed

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Favicon from "../public/logo.svg";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -29,6 +30,7 @@ export default function RootLayout({
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
           {children}
           <Toaster />
+          <SpeedInsights />
         </body>
       </html>
     </ClerkProvider>
