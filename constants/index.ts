d8428bcbb2104d9a5d2d8ff7341f645a2fc15dd5
ZiyaAkhundov <akhundovziya04@gmail.com>
2024-12-06 14:00:09 +0400

    prices

--- constants/index.ts
+++ constants/index.ts
@@ -71,7 +71,7 @@ export const plans = [
     _id: 2,
     name: "Pro Package",
     icon: "/assets/icons/free-plan.svg",
-    price: 40,
+    price: 9.99,
     credits: 40,
     inclusions: [
       {
@@ -96,7 +96,7 @@ export const plans = [
     _id: 3,
     name: "Premium Package",
     icon: "/assets/icons/free-plan.svg",
-    price: 80,
+    price: 12.99,
     credits: 100,
     inclusions: [
       {
