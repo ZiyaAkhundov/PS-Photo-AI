25405a2c5e789ae39def94e4e1ae868636207078
ZiyaAkhundov <akhundovziya04@gmail.com>
2024-12-06 11:28:59 +0400

    live chat

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,19 @@ export default function RootLayout({
           {children}
           <Toaster />
           <SpeedInsights />
+          {`<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6752a5152480f5b4f5a88429/1iedd9ng9';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->`}
         </body>
       </html>
     </ClerkProvider>
