7430f1572c2d86a657350b07ae5e52a9d5c55772
ZiyaAkhundov <akhundovziya04@gmail.com>
2024-12-06 11:33:05 +0400

    live chat script

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
 
 const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -31,19 +32,23 @@ export default function RootLayout({
           {children}
           <Toaster />
           <SpeedInsights />
-          {`<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/6752a5152480f5b4f5a88429/1iedd9ng9';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->`}
+          <Script
+            id="tawk-to-script"
+            strategy="afterInteractive" 
+            dangerouslySetInnerHTML={{
+              __html: `
+                var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/6752a5152480f5b4f5a88429/1iedd9ng9';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+                })();
+              `,
+            }}
+          />
         </body>
       </html>
     </ClerkProvider>
